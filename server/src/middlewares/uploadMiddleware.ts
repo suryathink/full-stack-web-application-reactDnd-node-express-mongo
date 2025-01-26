f868c82b489230f2ddde4276c644cc889c408d93
suryathink <suryaprakashsingh110@gmail.com>
2025-01-27 00:10:35 +0530

    fixes types

--- server/src/middlewares/uploadMiddleware.ts
+++ server/src/middlewares/uploadMiddleware.ts
@@ -1,11 +1,18 @@
-import multer from "multer";
+import { Request } from "express"; // Import Request from express
+import multer, { FileFilterCallback } from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import cloudinary from "../config/cloudinary";
+
+// Define the types for the file parameter
+interface MulterFile {
+  originalname: string;
+  mimetype: string;
+}
 
 // Configure storage for Multer to upload files to Cloudinary
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
-  params: async (req, file) => {
+  params: async (req: Request, file: MulterFile) => {
     return {
       folder: "feed_posts", // Cloudinary folder where files will be stored
       format: "png", // Optional: specify file format (e.g., "jpeg" or "png")
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,8 @@
-import { Express } from "express";
+import { Express, Request, Response } from "express";
 import { v1Apis } from "./v1";
 
 export const routes = function (app: Express) {
-  app.use("/health", (req, res) => {
+  app.use("/health", (req: Request, res: Response) => {
     res.send("Good Health");
     return;
   });
