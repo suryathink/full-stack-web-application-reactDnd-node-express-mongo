1b75b81af066689cc6181dcf28f2d277b17ef388
Surya Prakash Singh <suryaprakashsingh110@gmail.com>
2025-01-27 22:44:52 +0530

    Delete TODO.ts

--- TODO.ts
+++
@@ -1,7 +0,0 @@
-// TODO -> Interate email for reset password, forgot password
-// TODO -> remove blackisted token into service file
-// TODO -> google auth
-// TODO -> backend deployemnt/ render
-
-// TODO-> add ci/cd and deploy it to aws
-// TODO -> put yours backend deployed link
